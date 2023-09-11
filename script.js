3265e68d38ae8a33f69ea49a1d8a39d829a4d743
prithvijedipatil <prithvipatil09@gmail.com>
2023-09-11 12:17:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 const byteSize = (str) => {
   // write your code here
+	
+  const encoder = new TextEncoder();
+  const encodedString = encoder.encode(str);
+  return encodedString.length;
+
 };
 
 // Do not change the code below
